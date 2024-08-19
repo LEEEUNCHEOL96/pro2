7afe4885dad4610e5ea54a8238262a38e6e4a1ef
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-08-19 12:35:24 +0900

    1단계2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,14 @@
+import org.example.App;
+
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" == 게시판 앱 == ");
-        Scanner scanner = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
 
-        while (true){
-            System.out.print("명령) " );
-            String commnad = scanner.nextLine();
+        .new App(sc).run();
 
-            if (commnad.equals("종료")){
-                break;
-            }
-        }
+       sc.close();
     }
 }
