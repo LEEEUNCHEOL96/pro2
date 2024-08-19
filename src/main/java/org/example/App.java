bba349881baaad03509c1fc7d943fa8cfc5abcfa
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-08-19 12:43:45 +0900

    1-3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,8 +10,22 @@ public class App {
     }
 
     public void run() {
-        System.out.println("Enter something:");
-        String input = scanner.nextLine();
-        System.out.println("You entered: " + input);
+        System.out.println(" == 게시판 앱 ==");
+        while (true){
+            System.out.print("명령) ");
+            String command = scanner.nextLine().trim();
+
+            if (command.equals("종료")){
+                break;
+            }
+            else if (command.equals("등록")){
+                System.out.print("제목: ");
+                command = scanner.nextLine();
+                System.out.print("내용: ");
+                command = scanner.nextLine();
+                System.out.print("1번 게시글이 등록되었습니다.");
+                break;
+            }
+        }
     }
 }
