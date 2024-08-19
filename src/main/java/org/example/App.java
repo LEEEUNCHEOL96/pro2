08ff4d4a0c4f3aea2c7c54d630d11660de61b6c9
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-08-19 12:51:46 +0900

    1-4

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,29 +3,32 @@ package org.example;
 import java.util.Scanner;
 
 public class App {
-    private Scanner scanner;
+    Scanner sc;
 
     public App(Scanner scanner) {
-        this.scanner = scanner;
+        this.sc = scanner;
     }
 
     public void run() {
+        int lastid = 1;
         System.out.println(" == 게시판 앱 ==");
         while (true){
             System.out.print("명령) ");
-            String command = scanner.nextLine().trim();
+            String command = sc.nextLine().trim();
 
             if (command.equals("종료")){
                 break;
             }
             else if (command.equals("등록")){
                 System.out.print("제목: ");
-                command = scanner.nextLine();
+                command = sc.nextLine();
                 System.out.print("내용: ");
-                command = scanner.nextLine();
-                System.out.print("1번 게시글이 등록되었습니다.");
-                break;
+                command = sc.nextLine();
+                System.out.printf("%d 번 게시글이 등록되었습니다.\n",lastid);
+                lastid++;
+
             }
+
         }
     }
 }
