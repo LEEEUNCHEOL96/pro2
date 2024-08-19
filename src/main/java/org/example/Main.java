df8d38673b8a01793808a6bdd9264fdaa6ea5644
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-08-19 12:07:28 +0900

    init5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 20; i++) {
             if (i % 2 == 1) {
                 System.out.println(i);
             }
