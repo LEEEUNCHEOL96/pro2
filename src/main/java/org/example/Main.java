9e89b48c78b416c2d2762a99cf8ff995c322d585
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-08-19 11:51:18 +0900

    init2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi hello3333");
-
+        for (int i = 0; i < 11; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
