57780da21ab2754b9535017cd2a5b609f899809e
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-08-20 11:51:59 +0900

    1-7-1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -54,19 +54,36 @@ public class App {
 
                 String [] commandList = command.split("\\?",2); //정규표현식으로 \\를 사용해야한다
 
-                String actionCode = commandList[0];
+                String actionCode = commandList[0]; // {"삭제", "=1"} 으로 나뉜것을 actionCode 첫번째에 저장한다.
 
-                String [] paramsStr = commandList[1].split("=",2);
+                String [] paramsStr = commandList[1].split("=",2); // {"id" , "1"} 으로 나뉨
 
                 String key = paramsStr[0];
-                String value = paramsStr[1];
-                int idx = Integer.parseInt(value);
+                // 배열의 첫 번째 요소를 key에 저장합니다.
+                // 이 경우 "id"입니다. 이 변수는 파라미터의 키입니다.
 
-                for(int i=0; i < articleList.size(); i++){
-                    if (articleList.get(i).getId()== idx){
-                        articleList.remove(i);
+                String value = paramsStr[1];
+                // 배열의 두 번째 요소를 value에 저장합니다.
+                // 이 경우 "1"입니다. 이 변수는 파라미터의 값입니다.
+
+                int idx = Integer.parseInt(value);
+                // value 문자열을 정수로 변환하여 idx에 저장합니다.
+
+
+                Article article = null;
+
+                for(int i=0; i < articleList.size(); i++){  // 배열순회
+                    if (articleList.get(i).getId()== idx){  // 현재 인덱스 i의 게시글 ID가 idx와 일치하는 경우
+                        article =articleList.get(i);  // 게시글을 리스트에서 제거합니다.
                     }
                 }
+                if (article==null){  // 예외처리
+                    System.out.printf("%d번 게시물은 존재하지 않습니다.\n",idx);
+                }else {
+                    articleList.remove(article);
+                    System.out.printf("%d번 게시물이 삭제되었습니다.\n",idx);
+                }
+
                 System.out.printf("%d번 게시물이 삭제되었습니다.\n",idx);
 
             }
