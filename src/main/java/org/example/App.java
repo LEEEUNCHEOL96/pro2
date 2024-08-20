9b709a7a1faaf7519d5664c3efcab768f7ee0fab
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-08-20 10:24:21 +0900

    1-6

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,15 +5,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    Scanner sc;
+    Scanner sc;   // Scanner 타입의 sc 를 초기화
 
-    App (Scanner sc) {
+    App (Scanner sc) {  // 생성자 함수
         this.sc = sc;
     }
 
     void run () {
         int lastId = 1;
+
         List<Article> articleList = new ArrayList<>();
+
         System.out.println("== 게시판 앱 ==");
         while (true) {
             System.out.print("명령) ");
@@ -33,10 +35,12 @@ public class App {
 
                 System.out.printf("%d번 게시물이 등록되었습니다.\n", lastId); // 증감 처리
                 lastId++;
+
+
             } else if (command.equals("목록")) {
                 System.out.println("번호 / 제목 / 내용");
                 System.out.println("----------------------");
-                for (int i = 0; i < articleList.size(); i++) {
+                for (int i = articleList.size() -1 ; i >-1 ; i--) {
                     Article article = articleList.get(i);
                     System.out.printf("%d, %s, %s\n", article.getId(), article.getSubject(), article.getContent());
                 }
