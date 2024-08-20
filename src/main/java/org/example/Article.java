e7b1ad01bec2e20eedac2e581de1236d7fbd2ddc
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-08-20 10:31:07 +0900

    1-6

--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -4,12 +4,15 @@ public class Article {
     private int id;
     private String subject;
     private String content;
+    //필드 id,subject,content 초기화
 
     Article(int id, String subject, String content) {
         this.id = id;
         this.subject = subject;
         this.content = content;
     }
+    //생성자함수
+
 
     public int getId () {
         return this.id;
@@ -22,3 +25,4 @@ public class Article {
     }
 
 }
+// private 필드를 가져오기 위해 getter을 만든다.
